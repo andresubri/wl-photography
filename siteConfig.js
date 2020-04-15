5ce59fb55afa14bcf3ae7bbba78868dac3f22cf7
WaldysLantigua <wlfotografia@outlook.es>
2020-04-15 00:57:47 -0400

    Prueba desde Desktop

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Waldys Lantigua Fotografia',
   shortName: '',
-  description: 'Buenas Noches',
+  description: 'Buenas Noches, gracias carlos por todo',
   url: 'https://www.jooplaan.com', // Domain of your site without prefix!
   prefix: '/',
   author: 'jooplaan',
