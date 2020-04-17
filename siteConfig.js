4f034b0fd539d0e07509b51b9d70c291d71020a4
WaldysLantigua <63695951+WaldysLantigua@users.noreply.github.com>
2020-04-17 00:24:29 -0400

    Prueba de Descripcion

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,15 @@
 module.exports = {
   name: 'Waldys Lantigua Fotografia',
   shortName: '',
-  description: 'Buenas Noches, gracias carlos por todo',
+  description: 'Waldys Lantigua 
+
+Hola! , desde niño he descubierto lo que realmente me apasiona, la fotografía,  y estoy encantado de que me ayudes a seguir haciendo de mis sueños una realidad.
+
+Soy un fotógrafo autodidacta desde el año 2015 , mercadólogo egresado del Tep-Pucmm.
+
+La fotografía es mi pasión, me permite contar historias a través de imágenes y me satisface saber que esas imagenes se convertirán en recuerdos los cuales ustedes atesoran.
+
+Las bodas tienen algo muy especial, son una celebración de amor y mi trabajo es poder plasmar en imágenes esa atmósfera tan especial que se produce en su gran dia.',
   url: 'https://www.jooplaan.com', // Domain of your site without prefix!
   prefix: '/',
   author: 'jooplaan',
